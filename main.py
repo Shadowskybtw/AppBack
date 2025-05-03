59b73d3cd8410267f4bd262ffee52b854777d0b1
Shadowsky <shadowskybtw@gmail.com>
2025-05-04 02:18:43 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from contextlib import asynccontextmanager
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from models import init_db
+import requests as rq
+
+ 
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    await init_db()
+    print('Bot Soset')
+    yield
+
+
+app = FastAPI(title="Stock App", lifespan=lifespan)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+@app.get("api/stocks/{tg_id}")
+async def stocks(tg_id: int):
+    user = await rq.add_user(tg_id)
+    return await rq.get_stocks(user.id)
+ 
+
+@app.get("/api/main/{tg_id}")
+async def profile(tg_id: int):
+    user = await rq.add_user(tg_id)
+    completed_stocks_count = await rq.get_completed_stocks_count(user.id)
+    return {'completedStocks': completed_stocks_count}
---
+++ requests.py
@@ -0,0 +1,44 @@
+from sqlalchemy import select, update, delete, func
+from models import async_session, User, Stock
+from pydantic import BaseModel, ConfigDict
+from typing import List
+
+
+class StockSchema(BaseModel):
+    id: int
+    title: str
+    completed: bool
+    user: int
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+async def add_user(tg_id):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        if user:
+            return user
+        
+        new_user = User(tg_id=tg_id)
+        session.add(new_user)
+        await session.commit()
+        await session.refresh(new_user)
+        return new_user
+
+
+async def get_stocks(user_id):
+    async with async_session() as session:
+        stocks = await session.scalars(
+            select(Stock).where(Stock.user == user_id, Stock.completed == False)
+        )
+
+        serialized_stocks = [
+            StockSchema.model_validate(t).model_dump() for t in stocks
+        ]
+        return serialized_stocks
+    
+
+async def get_completed_stocks_count(user_id):
+    async with async_session() as session:
+         return await session.scalar(select(func.count(Stock.id)).where(Stock.completed == True))
+    
