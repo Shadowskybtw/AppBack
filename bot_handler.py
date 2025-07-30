dfb3c818907847e89f7adf9fcb521c4e8ee794f6
Shadowsky <shadowskybtw@gmail.com>
2025-07-30 21:33:53 +0400

    Изменения

--- bot_handler.py
+++ bot_handler.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import requests
 import os
 
-BOT_TOKEN = os.getenv("7829386579:AAGAUFZdd6PbuDtdEI1zxAkfY1vlj0Mu0WE")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 BACKEND_URL = "https://refactored-cod-v6ww469vp657fwqpw-8000.app.github.dev/"  # Заменить на домен своего backend
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
