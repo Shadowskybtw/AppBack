daac130029b8e140656e8d092421dd87632eb112
Shadowsky <shadowskybtw@gmail.com>
2025-07-31 00:10:45 +0400

    Изменения

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from fastapi import Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from models import init_db
-import services as rq
+import rq as rq
 
 from fastapi.responses import JSONResponse
 
