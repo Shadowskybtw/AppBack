558bd595800523040e698e306c6f3b6240989d39
Shadowsky <shadowskybtw@gmail.com>
2025-07-31 20:27:29 +0400

    Изменения

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI(title="Stock App", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['https://frontend-delta-sandy-58.vercel.app'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
